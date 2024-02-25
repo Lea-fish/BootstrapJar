9a94947a065442efac8a3b49df7a8904f38f3721
threadexception <hannes.gaumann@outlook.de>
2024-02-25 11:37:30 +0100

    Make it start in launcher

--- src/de/leafish/Main.java
+++ src/de/leafish/Main.java
@@ -1,6 +1,8 @@
-package de.terrarier;
+package de.leafish;
 
-public class LeafLaunch {
+public class Main {
+
+    // https://bugs.mojang.com/browse/MCL-23639
 
     public static void main(String[] args) {
         for (String arg : args) {
@@ -10,6 +12,8 @@ public class LeafLaunch {
         // FIXME: for now just include the Leafish binary in the jar and run it (as a sub process)
         // FIXME: but in the future we would want to check the most recent version on github and
         // FIXME: download it if necessary
+
+        while(true) {}
     }
 
 }
